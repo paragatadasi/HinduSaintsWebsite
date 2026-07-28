28f5183d354570757b0a3b638b011abecfd86ac0
Anjaly Poruthoor <aporu@DESKTOP-PQB9RLN.mit.edu>
2026-07-28 22:33:40 +0200

    Open legal footer links in new tabs

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -8,8 +8,14 @@ export async function SiteFooter() {
       <div className="page-shell">
         <p className="site-footer__legal">
           {content.copyright}{" "}
-          <a href={content.imprint.href}>{content.imprint.label}</a>.{" "}
-          <a href={content.privacyPolicy.href}>{content.privacyPolicy.label}</a>.
+          <a href={content.imprint.href} target="_blank" rel="noopener noreferrer">
+            {content.imprint.label}
+          </a>
+          .{" "}
+          <a href={content.privacyPolicy.href} target="_blank" rel="noopener noreferrer">
+            {content.privacyPolicy.label}
+          </a>
+          .
         </p>
       </div>
     </footer>
