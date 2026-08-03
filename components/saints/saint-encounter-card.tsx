a53b3a5bc8f450ef4e14706f310aab43677bc4d5
Anjaly Poruthoor <aporu@DESKTOP-PQB9RLN.mit.edu>
2026-08-03 03:31:42 +0200

    Stabilize random saint encounters

--- components/saints/saint-encounter-card.tsx
+++ components/saints/saint-encounter-card.tsx
@@ -21,7 +21,7 @@ export function SaintEncounterCard({ variant }: SaintEncounterCardProps) {
         <h2 id={titleId}>Encounter a New Saint</h2>
       )}
       <p>Allow providence to introduce you to a saint you may never have met before.</p>
-      <Button href="/saints/random" icon={<ArrowRight size={16} />} iconPosition="end">
+      <Button href="/saints/random" icon={<ArrowRight size={16} />} iconPosition="end" prefetch={false}>
         Begin the encounter
       </Button>
     </aside>
